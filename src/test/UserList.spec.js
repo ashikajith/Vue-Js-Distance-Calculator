d0290ba31928d89e1b4d97129c1b3b6f7597477f
ashikajith <ashikajith@gmail.com>
2018-01-12 16:41:37 +0530

    Added test for the filtered Items

--- src/test/UserList.spec.js
+++ src/test/UserList.spec.js
@@ -1,72 +1,81 @@
+import { shallow } from 'vue-test-utils'
 import Vue from 'vue'
 import App from '../App'
 import UserList from '../components/UserList'
 
 describe('Userlist.spec.js', () => {
-  let cmp, vm, toLatitude, toLongitude, distance
+  let cmp, toLatitude, toLongitude, distance
   beforeEach(()=>{
     toLatitude = 52.3191841;
     toLongitude = -8.5072391;
     distance = 189;
-    cmp = Vue.extend(UserList)
-    vm = new cmp({
-      data:{
-        userData: [
-          {"latitude": "53.807778", "user_id": 28, "name": "Charlie Halligan", "longitude": "-7.714444"},
-          {"latitude": "52.3191841", "user_id": 3, "name": "Jack Enright", "longitude": "-8.5072391"},
-          {"latitude": "51.8856167", "user_id": 2, "name": "Ian McArdle", "longitude": "-10.4240951"}
-        ]
-      }
-    }).$mount()
+    cmp = shallow(UserList, {
+    })
   })
 
   it ('should list users', () => {
-    expect(vm.userData.length).toEqual(3);
+    expect(cmp.vm.userData.length).toEqual(32);
   });
 
   describe('calculateDistance function', () => {
     it ('should return the distance between two co-ordinates', () => {
-      expect(vm.calculateDistance(toLatitude, toLongitude)).toEqual(189.0);
+      expect(cmp.vm.calculateDistance(toLatitude, toLongitude)).toEqual(189.0);
     });
 
     it ('should return the distance in string format', () => {
-      var distance = vm.calculateDistance(toLatitude, toLongitude);
+      var distance = cmp.vm.calculateDistance(toLatitude, toLongitude);
       expect(typeof(distance)).toEqual("number");
     });
   });
 
   describe('lessThanHundred function', () => {
     it (' should return a boolean value', () => {
-      var bool = vm.lessThanHundred(toLatitude, toLongitude);
+      var bool = cmp.vm.lessThanHundred(toLatitude, toLongitude);
       expect(typeof(bool)).toEqual('boolean');
     });
 
     it ('should return false if the distance between co-ordinates is more than 100', () => {
-      var bool = vm.lessThanHundred(toLatitude, toLongitude);
+      var bool = cmp.vm.lessThanHundred(toLatitude, toLongitude);
       expect(bool).toEqual(false);
     });
 
     it ('should return false if the distance between co-ordinates is more than 100', () => {
       toLatitude = 53.2451022;
       toLongitude = -6.238335;
-      var bool = vm.lessThanHundred(toLatitude, toLongitude);
+      var bool = cmp.vm.lessThanHundred(toLatitude, toLongitude);
       expect(bool).toEqual(true);
     });
   });
 
-  describe('sortedItems() computed method', () => {
-    it ('should returns userData object', () => {
-      expect(typeof(vm.sortedItems)).toEqual('object');
+  describe('sortedItems ', () => {
+    it ('should return userData object', () => {
+      expect(typeof(cmp.vm.sortedItems)).toEqual('object');
     });
 
-    it ('should returns total array elements', () => {
-      expect(vm.sortedItems.length).toEqual(3);
+    it ('should return total array elements', () => {
+      expect(cmp.vm.sortedItems.length).toEqual(32);
+    });
+
+    it ('should return object in the order of user_id', () => {
+      expect(cmp.vm.sortedItems[0].user_id).toEqual(1);
     });
   });
 
-  describe('filteredItems() computed method', () => {
-    it ('should returns userData object', () => {
-      expect(typeof(vm.filteredItems)).toEqual('object');
+  describe('filteredItems ', () => {
+    it ('should return userData object', () => {
+      expect(typeof(cmp.vm.filteredItems)).toEqual('object');
+    });
+
+    it ('should return userData which are at a distance less than 100 km', () => {
+      expect((cmp.vm.filteredItems.length)).toEqual(16);
+    });
+  });
+
+  describe('getItemList ', () => {
+    it ('should call sortedItems if filteredData is false', () => {
+      expect(cmp.vm.filteredData).toEqual(false)
+      let spy = jest.spyOn(cmp.vm, "sortedItems")
+      expect(spy).toBeCalled();
     });
   });
 });
